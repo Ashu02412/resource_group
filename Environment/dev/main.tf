949fde50a986ada78511e6987ecc2b9e2c751833
Ashu02412 <ashu.dcit024@gmail.com>
2026-08-18 17:38:56 +0530

    ashu

--- Environment/dev/main.tf
+++ Environment/dev/main.tf
@@ -1,10 +1,10 @@
 module "rg" {
-    source = "../../landing_zone/resource_group"
-    rg = var.rg
-  
+  source = "../../landing_zone/resource_group"
+  rg     = var.rg
+
 }
 module "vnet" {
-  source = "../../module/vnet"
-  depends_on = [ module.rg ]
-  vnet = var.vnet
+  source     = "../../module/vnet"
+  depends_on = [module.rg]
+  vnet       = var.vnet
 }
